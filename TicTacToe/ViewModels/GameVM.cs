1c34a9ce333d22893ae794e1b93315b86bab923d
Pamfile Alex <damrosrak@gmail.com>
2021-04-26 11:48:00 +0300

    Added menu bar visuals

---
+++ TicTacToe/ViewModels/GameVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TicTacToe.Models;
+
+namespace TicTacToe.ViewModels
+{
+    class GameVM
+    {
+        public Game Game { get; }
+        public List<List<CellVM>> GameBoard { get; }
+
+        public GameVM()
+        {
+            Game = new Game();
+
+        }
+
+        private List<List<CellVM>> CellBoardToCellVMBoard(List<List<Cell>> gameBoard)
+        {
+            return gameBoard.Select(row => row.Select(cell => new CellVM()).ToList()).ToList();
+        }
+    }
+}
