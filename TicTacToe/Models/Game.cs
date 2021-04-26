eeac7ae65030294e7799b9d85bf6f974d397125d
Pamfile Alex <damrosrak@gmail.com>
2021-04-26 12:25:39 +0300

    Implemented Game class and added GameLogic Singleton

--- TicTacToe/Models/Game.cs
+++ TicTacToe/Models/Game.cs
@@ -4,15 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using TicTacToe.ViewModels;
+
 namespace TicTacToe.Models
 {
-    class Game
+    class Game : BaseNotification
     {
         public List<List<Cell>> GameBoard { get; }
+        public Cell.Type turn;
+        public Cell.Type Turn
+        {
+            get { return turn; }
+            set
+            {
+                turn = value;
+                NotifyPropertyChanged("Player");
+            }
+        }
+
+        public String Player
+        {
+            get
+            {
+                return Turn == Cell.Type.None ? "Error" : Turn == Cell.Type.X ? "Player X" : "Player O";
+            }
+        }
 
         public Game(List<List<Cell>> gameBoard)
         {
             this.GameBoard = gameBoard;
+            this.Turn = Cell.Type.X;
         }
     }
 }
---
+++ TicTacToe/Utils/GameLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Utils
+{
+    class GameLogic
+    {
+        public static GameLogic Instance { get; } = new GameLogic();
+
+        static GameLogic() { }
+        private GameLogic() { }
+    }
+}
